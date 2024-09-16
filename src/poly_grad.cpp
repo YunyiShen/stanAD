7caf96a18b1477477b52cfb20f4ade9b069ede75
Yunyi Shen <yshen99@outlook.com>
2024-09-16 14:15:18 -0400

    only grad work so far...

--- src/poly_grad.cpp
+++ src/poly_grad.cpp
@@ -6,6 +6,8 @@
 #include <stan/math.hpp>     // finally pull in everything from rev/ and prim/
 #include <Rcpp.h>
 #include <RcppEigen.h>       // do this AFTER including stuff from stan/math
+//using namespace stan;
+//using namespace stan::math;
 
 // [[Rcpp::plugins(cpp17)]]
 
@@ -21,14 +23,16 @@ Eigen::MatrixXd H(Eigen::VectorXd x, Eigen::VectorXd a) { // Hessian by AD using
 }
 
 
-// [[Rcpp::export]]
-Eigen::VectorXd log_cholesky_grad(const Eigen::VectorXd& l_params, const Eigen::MatrixXd& A) {
+
+Eigen::Matrix<stan::math::var_value<double>, -1, 1> log_cholesky_grad(Eigen::Matrix<stan::math::var_value<double>, Eigen::Dynamic, 1> l_params, const Eigen::MatrixXd& A) {
   int n = A.rows(); // assuming A is square
 
   // Function to calculate Tr(LL^T A) with log-Cholesky parameterization
-  auto func = [&](const auto& l_params) -> auto {
+  auto func = [&](auto &l_params) -> auto {
     // Use Eigen matrix with stan::math::var type to support autodiff
-    Eigen::Matrix<stan::math::var, Eigen::Dynamic, Eigen::Dynamic> L(n, n);
+    //Eigen::Matrix<stan::math::var, Eigen::Dynamic, Eigen::Dynamic> L(n, n);
+    Eigen::Matrix<stan::math::fvar<stan::math::var_value<double> >, -1, -1> L(n,n);
+    
     L.setZero();
 
     // Fill the lower triangular matrix L with the log-Cholesky parameterization
@@ -36,23 +40,23 @@ Eigen::VectorXd log_cholesky_grad(const Eigen::VectorXd& l_params, const Eigen::
     for (int i = 0; i < n; ++i) {
       for (int j = 0; j <= i; ++j) {
         if (i == j) {
-          L(i, j) = stan::math::exp(l_params(index)); // exponentiate diagonal elements
+          L(i, j) =  stan::math::exp(l_params(index,0)); // exponentiate diagonal elements
         } else {
-          L(i, j) = l_params(index); // off-diagonal elements remain the same
+          L(i, j) = l_params(index,0); // off-diagonal elements remain the same
         }
         index++;
       }
     }
 
     // Compute the trace of L * L^T * A
-    Eigen::Matrix<stan::math::var, Eigen::Dynamic, Eigen::Dynamic> LLT = L * L.transpose();
-    stan::math::var result = (LLT.cwiseProduct(A)).sum(); // Tr(LL^T A)
+    Eigen::Matrix<stan::math::fvar<stan::math::var_value<double> >, Eigen::Dynamic, Eigen::Dynamic> LLT = L * L.transpose();
+    stan::math::fvar<stan::math::var_value<double> > result = (LLT.cwiseProduct(A)).sum(); // Tr(LL^T A)
     return result;
   };
 
   // Prepare storage for gradient
-  double fx;
-  Eigen::VectorXd grad_fx;
+  stan::math::var_value<double> fx;
+  Eigen::Matrix<stan::math::var_value<double> , -1, 1> grad_fx;
 
   // Compute gradient
   stan::math::gradient(func, l_params, fx, grad_fx);
@@ -62,14 +66,16 @@ Eigen::VectorXd log_cholesky_grad(const Eigen::VectorXd& l_params, const Eigen::
 
 
 
-// [[Rcpp::export]]
-Eigen::MatrixXd log_cholesky_H(const Eigen::VectorXd& l_params, const Eigen::MatrixXd& A) {
+
+Eigen::Matrix< stan::math::fvar<stan::math::fvar<stan::math::fvar<stan::math::var_value<double> >>>, -1,-1 > log_cholesky_H(Eigen::Matrix<stan::math::fvar<stan::math::fvar<stan::math::var_value<double> >>, Eigen::Dynamic, 1> l_params, const Eigen::MatrixXd& A) {
   int n = A.rows(); // assuming A is square
 
   // Function to calculate Tr(LL^T A) with log-Cholesky parameterization
-  auto func = [&](const auto& l_params) -> auto {
+  auto func = [&](auto &l_params) -> auto {
     // Use Eigen matrix with stan::math::var type to support autodiff
-    Eigen::Matrix<stan::math::var, Eigen::Dynamic, Eigen::Dynamic> L(n, n);
+    //Eigen::Matrix<stan::math::var, Eigen::Dynamic, Eigen::Dynamic> L(n, n);
+    Eigen::Matrix<stan::math::fvar<stan::math::fvar<stan::math::fvar<stan::math::fvar<stan::math::var_value<double> >>>>, -1, -1> L(n,n);
+    
     L.setZero();
 
     // Fill the lower triangular matrix L with the log-Cholesky parameterization
@@ -77,24 +83,24 @@ Eigen::MatrixXd log_cholesky_H(const Eigen::VectorXd& l_params, const Eigen::Mat
     for (int i = 0; i < n; ++i) {
       for (int j = 0; j <= i; ++j) {
         if (i == j) {
-          L(i, j) = stan::math::exp(l_params(index)); // exponentiate diagonal elements
+          L(i, j) =  stan::math::exp(l_params(index,0)); // exponentiate diagonal elements
         } else {
-          L(i, j) = l_params(index); // off-diagonal elements remain the same
+          L(i, j) = l_params(index,0); // off-diagonal elements remain the same
         }
         index++;
       }
     }
 
     // Compute the trace of L * L^T * A
-    Eigen::Matrix<stan::math::var, Eigen::Dynamic, Eigen::Dynamic> LLT = L * L.transpose();
-    stan::math::var result = (LLT.cwiseProduct(A)).sum(); // Tr(LL^T A)
+    Eigen::Matrix<stan::math::fvar<stan::math::fvar<stan::math::var_value<double> > >, Eigen::Dynamic, Eigen::Dynamic> LLT = L * L.transpose();
+    stan::math::fvar<stan::math::fvar<stan::math::var_value<double> >> result = (LLT.cwiseProduct(A)).sum(); // Tr(LL^T A)
     return result;
   };
 
   // Prepare storage for gradient
-  stan::math::var fx;
-  Eigen::Matrix< stan::math::var, Eigen::Dynamic, 1 > grad_fx;
-  Eigen::Matrix< stan::math::var, Eigen::Dynamic, Eigen::Dynamic >  hess_fx;
+  stan::math::fvar<stan::math::fvar<stan::math::var_value<double> >> fx;
+  Eigen::Matrix<stan::math::fvar<stan::math::fvar<stan::math::var_value<double> > >, -1, 1> grad_fx;
+  Eigen::Matrix< stan::math::fvar<stan::math::fvar<stan::math::var_value<double> > >, Eigen::Dynamic, Eigen::Dynamic >  hess_fx;
 
   // Compute gradient
   stan::math::hessian(func, l_params, fx, grad_fx, hess_fx);
